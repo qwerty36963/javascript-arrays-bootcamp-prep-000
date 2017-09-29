6e0a78c50cd4fdd03719dd95aa8c9eb5097220c3
qwerty36963 <sparks21012@gmail.com>
2017-09-29 15:07:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,3 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function AddElementToBeginningOfArray([1,]'foo'){
- AddElementToBeginningOfArray.unshift('foo')
 
-}return AddElementToBeginningOfArray()
