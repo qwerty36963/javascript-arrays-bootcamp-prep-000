9e5317dc249e7a56f0935d0283f4de97be243eba
qwerty36963 <sparks21012@gmail.com>
2017-09-29 16:31:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars, foo){
   return chocolateBars
 }
 
-function accessElementInArray(chocolateBars, index){
-  console.log(chocolateBars[1,2,3],index)
+function accessElementInArray(array, index){
+  return array[index];
 }
