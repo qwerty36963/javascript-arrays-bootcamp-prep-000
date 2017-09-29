cfae704d85b1bb95d097ab641497c9186fc41223
qwerty36963 <sparks21012@gmail.com>
2017-09-29 16:37:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
 chocolateBars.shift()
 return chocolateBars
 }
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  
+}
