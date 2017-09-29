971008b668b3a9e50c85e49e3b001b3302b1f539
qwerty36963 <sparks21012@gmail.com>
2017-09-29 17:06:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(choclateBars){
   return choclateBars
 }
 function removeElementFromEndOfArray(choclateBars){
-  choclateBars = choclateBars.slice(0,choclateBars.length - 3)
+  choclateBars = choclateBars.slice(0,choclateBars.length - 1)
   return choclateBars
 }
